e229d6555cfa40086943deea0985383d38424db7
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-17 18:00:58 +0200

    added for cycle in playButton eventListener

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 const playButtonEl = document.querySelector('header button#play');
 const mainEl = document.querySelector('main');
 
-console.log(playButtonEl, mainEl);
 playButtonEl.addEventListener('click', function(){
     console.log('click');
-    mainEl.innerHTML = '<section id="grid" class="flex-centered">'
+    mainEl.innerHTML = '<section id="grid" class="flex-centered">';
+    const gridEl = document.querySelector('#grid');
+    console.log(gridEl);
+
+    // > ciclo for per creare le celle e il loro contenuto
+    for(let index = 0; index < 100; index++){
+        const articleEl = document.createElement('article');
+        articleEl.classList.add('square', 'flex-centered');
+        const spanEl = document.createElement('span');
+        spanEl.append(index + 1);
+        articleEl.appendChild(spanEl)
+
+        // > la cella deve essere cliccabile
+        articleEl.addEventListener('click', function(){
+            articleEl.classList.toggle('active');
+        })
+
+        gridEl.appendChild(articleEl);
+    }
+    
 })
 
 
